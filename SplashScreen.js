c08089ca1b16503508156538a66c7e6a2753a410
Liu@iMac <pizza@pizzaliu.com>
2016-06-14 15:17:52 +0800

    get splash from zhihu api

--- SplashScreen.js
+++ SplashScreen.js
@@ -1,16 +1,4 @@
-/*
-** TODO
-
-- 无网络： 则载入现成图片
-- 有网络
-    尝试加载网络图片
-    如果加载失败或加载超时，则载入现成图片
-    如果加载成功，则载入，并保存到本地
-
-* 现成图片：之前载入过的图片/默认图片
-
-*/
-
+'use strict';
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -22,6 +10,9 @@ import {
   Animated,
 } from 'react-native';
 
+import ZhihuApi from './ZhihuApi';
+
+var zhihu_api = new ZhihuApi();
 var WINDOW_WIDTH = Dimensions.get('window').width;
 var WINDOW_HEIGHT = Dimensions.get('window').height;
 
@@ -48,18 +39,51 @@ const styles = StyleSheet.create({
 
 class SplashScreen extends React.Component {
   static defaultProps = {
-    source: require('./images/splash.jpg'),//{uri: 'https://pic4.zhimg.com/3a811bb46a42d6251529e70ce3b7153f.jpg'},
-    text: '© zhi.hu',
+    source: null,
+    text: '',
   };
+
+  initStartImage() {
+    var startImage = null;
+    if(this.props.source) {
+      startImage = {
+        source: this.props.source,
+        text: this.props.text,
+      };
+      this.setState({startImage: startImage});
+    } else {
+      zhihu_api.getStartImage()
+        .then((result) => {
+          if (result){
+            startImage = {
+              source: {uri: result.img},
+              text: result.text
+            };
+            console.log(startImage);
+            this.setState({startImage: startImage});
+          }
+        })
+        .catch((error) => {
+          //console.error(error);
+        })
+        .done();
+    }
+    zhihu_api.updateStartImage();
+  }
 
   constructor(props) {
     super(props);
     this.state = {
+      startImage: {
+        source: require('./images/splash.jpg'),//{uri: 'https://pic4.zhimg.com/3a811bb46a42d6251529e70ce3b7153f.jpg'},
+        text: '© zhi.hu',
+      },
       bounceValue: new Animated.Value(1),
     };
   }
 
   componentDidMount() {
+    this.initStartImage();
     this.state.bounceValue.setValue(1);
     Animated.timing(
       this.state.bounceValue,
@@ -74,13 +98,13 @@ class SplashScreen extends React.Component {
     return (
       <View style={styles.container}>
         <Animated.Image
-          source={this.props.source}
+          source={this.state.startImage.source}
           style={[styles.image, {
             transform: [
               {scale: this.state.bounceValue},
             ]
           }]} />
-          <Text style={styles.text}>{this.props.text}</Text>
+          <Text style={styles.text}>{this.state.startImage.text}</Text>
       </View>
     );
   }
