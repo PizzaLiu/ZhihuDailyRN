47d1b1fc4c286ff1e0c9c2a045113adcf874570d
Liu@iMac <pizza@pizzaliu.com>
2016-07-06 09:18:02 +0800

    [android] add theme remove & add icon

--- MainScreen.android.js
+++ MainScreen.android.js
@@ -39,11 +39,6 @@ const styles = StyleSheet.create({
 
 var DRAWER_REF = 'drawer';
 var DRAWER_WIDTH = Dimensions.get('window').width - 60;
-var toolbar_actions = [
-  {title: '提醒', icon: require('./images/ic_message_white.png'), show: 'always'},
-  {title: '夜间模式', show: 'never'},
-  {title: '设置选项', show: 'never'},
-];
 
 export default class MainScreen extends Component {
     static propTypes = {
@@ -53,7 +48,31 @@ export default class MainScreen extends Component {
         super(props);
         this.state = {
             toolbarTitle: '首页',
+            theme: null,
+            actions: [],
         };
+    }
+
+    _genToolbarActions = () => {
+      var actions = [];
+      var toolbar_actions = [
+        {title: '提醒', icon: require('./images/ic_message_white.png'), show: 'always'},
+        {title: '夜间模式', show: 'never'},
+        {title: '设置选项', show: 'never'},
+      ];
+      var toolbar_actions_follow = [
+        {title: '添加收藏', icon: require('./images/ic_theme_add.png'), show: 'always'},
+      ];
+      var toolbar_actions_del = [
+        {title: '删除收藏', icon: require('./images/ic_theme_remove.png'), show: 'always', pizza: function() {}},
+      ];
+      if(!this.state.theme) {
+        actions = toolbar_actions;
+      } else {
+        actions = this.state.theme.subscribed ? toolbar_actions_del : toolbar_actions_follow;
+      }
+      this.state.actions = actions;
+      return actions;
     }
 
     _renderDrawerView = () => {
@@ -72,13 +91,15 @@ export default class MainScreen extends Component {
       this.refs[DRAWER_REF].openDrawer();
     }
 
-    _onActionSelected = () => {
+    _onActionSelected = (actionOffset) => {
       // TODO
+      console.log(this.state.actions[actionOffset]);
     }
 
     _onThemeSelected = (theme) => {
       // TODO
       this.refs[DRAWER_REF].closeDrawer();
+      this.setState({theme: theme});
     }
 
     render() {
@@ -94,9 +115,9 @@ export default class MainScreen extends Component {
               navIcon = {require('./images/ic_menu_white.png')}
               style = {styles.toolbar}
               onIconClicked = {this._showDrawer}
-              title = {this.state.toolbarTitle}
+              title = {this.state.theme ? this.state.theme.name : this.state.toolbarTitle}
               titleColor = 'white'
-              actions = {toolbar_actions}
+              actions = {this._genToolbarActions()}
               overflowIcon = {require('./images/ic_menu_moreoverflow.png')}
               onActionSelected={this._onActionSelected}
             />
